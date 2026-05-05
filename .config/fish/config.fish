9cab199e1d177a6fc60cc41354ff42c6f6b616ba
Raul Bernardeli <xraull@gmail.com>
2026-05-05 10:03:34 -0300

    fish: enable nerd fonts, add alt+arrow word navigation; ghostty: set JetBrains Mono Nerd Font; htop: enable tree view

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,7 @@ export LANG=en_US.UTF-8
 fish_add_path /opt/homebrew/bin
 
 # Bobthefish theme settings
-set -g theme_nerd_fonts no
-set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
 
 # Default editor
 set -x EDITOR nvim
@@ -17,6 +16,10 @@ alias ls="lsd"
 alias ll="lsd --long -h --group-dirs=first --git -A"
 alias la="lsd -la"
 alias lt="lsd --tree"
+
+# Navegação por palavras com Left Alt + Seta
+bind \e\[1\;3D backward-word
+bind \e\[1\;3C forward-word
 
 # Java
 set -x JAVA_HOME /opt/homebrew/opt/openjdk@17
